363b03e08c28ecd7793c3d1a685a6d358f53bbee
IlianDev <ilian.g.kostov@gmail.com>
2022-07-04 22:49:41 +0300

    task

--- Python Fundamentals - January 2021/1-BASIC-SYNTAX-CONDITIONAL-STATEMENTS-AND-LOOPS/Excercise/08. Mutate Strings.py
+++ Python Fundamentals - January 2021/1-BASIC-SYNTAX-CONDITIONAL-STATEMENTS-AND-LOOPS/Excercise/08. Mutate Strings.py
@@ -1,16 +1,32 @@
 string_1 = input()
-string_2 = input()
+# string_2 = input()
+#
+# current_string = ""
+# previous_string = string_1
+#
+# for iteration in range(len(string_1)):
+#     for index_str_2 in range(0, iteration + 1):
+#         current_string += string_2[index_str_2]
+#     for index_str_1 in range(iteration + 1, len(string_1)):
+#         current_string += string_1[index_str_1]
+#     if not previous_string == current_string:
+#         print(current_string)
+#     # prepare date for the next iteration
+#     previous_string = current_string
+#     current_string = ""
 
-current_string = ""
-previous_string = string_1
+starting_word = input()
+aim_mutated_word = input()
 
-for iteration in range(len(string_1)):
-    for index_str_2 in range(0, iteration + 1):
-        current_string += string_2[index_str_2]
-    for index_str_1 in range(iteration + 1, len(string_1)):
-        current_string += string_1[index_str_1]
-    if not previous_string == current_string:
-        print(current_string)
-    # prepare date for the next iteration
-    previous_string = current_string
-    current_string = ""
+result = ""
+previous_str = starting_word
+
+for index in range(len(starting_word)):
+    for i in range(index + 1):
+        result += aim_mutated_word[i]
+    for i in range(index + 1, len(aim_mutated_word)):
+        result += starting_word[i]
+    if not result == previous_str:
+        print(result)
+    previous_str = result
+    result = ""
