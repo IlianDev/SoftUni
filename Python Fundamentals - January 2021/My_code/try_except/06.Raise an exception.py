d71f97b232eadc924a228ad71b4b724a273363f7
IlianDev <ilian.g.kostov@gmail.com>
2022-07-12 02:33:04 +0300

    examples

---
+++ Python Fundamentals - January 2021/My_code/try_except/06.Raise an exception.py
@@ -0,0 +1,13 @@
+"""
+throw an exception if a condition occurs.
+To throw (or raise) an exception, we use the raise keyword.
+
+Example
+Raise an error and stop the program if x is lower than 0:
+"""
+
+x = -1
+
+if x < 0:
+  raise Exception("Sorry, no numbers below zero")
+
