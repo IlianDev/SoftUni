7ce6a760b09f83951daa0098d4c4104f00aa9013
Ilian Kostov <63207846+IlianDev@users.noreply.github.com>
2023-09-23 13:06:26 +0300

    Create example.sql

---
+++ Python DB - September 2023/1_Data_Types_Working_with _Tables/example.sql
@@ -0,0 +1,21 @@
+/*
+CREATE TYPE adress as(
+	street TEXT,
+	city TEXT,
+	postalCode CHAR(4)
+);
+
+CREATE TABLE cusomers(
+	id serial PRIMARY KEY,
+	customer_name TEXT,
+	customer_adress adress
+);
+*/
+
+INSERT INTO
+	customers(customer_name, customer_addres)
+VALUES
+	('Diyan',('somes street', 'Sofia', '1618'));
+
+
+
