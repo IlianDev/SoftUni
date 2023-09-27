756811920cb36c2334a45816be8bd4af99059f0e
Ilian Kostov <63207846+IlianDev@users.noreply.github.com>
2023-09-27 23:47:44 +0300

    Update 5_order.sql

--- Python DB - September 2023/2_Basic_CRUD/Lab/Examples/5_order.sql
+++ Python DB - September 2023/2_Basic_CRUD/Lab/Examples/5_order.sql
@@ -1,16 +1,6 @@
-SELECT
-	*
-FROM
-	employees
-ORDER by first_name -- ASC or DESC
-
-
-SELECT
-	first_name,
-	last_name,
-	job_title,
-	salary
-From
-	employees
-ORDER BY
-	salary ASC
+SELECT first_name,
+       last_name,
+       job_title,
+       salary
+From employees
+ORDER BY salary ASC
