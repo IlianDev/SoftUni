ab20e47ee1a4cd91a15877a41fc06e331cb828d8
Ilian Kostov <63207846+IlianDev@users.noreply.github.com>
2023-09-30 15:15:30 +0300

    Update 4_Limit_records.sql

--- Python DB - September 2023/2_Basic_CRUD/Exercise/4_Limit_records.sql
+++ Python DB - September 2023/2_Basic_CRUD/Exercise/4_Limit_records.sql
@@ -1,3 +1,10 @@
+/*
+ Develop a SQL query that selects from the "employees" table "id", "first_name", "last_name", and
+
+"job_title". Combine the "first_name" and "last_name" fields into a single field called "Full Name". Rename the "id"
+ column as "ID" and the "job_title" column as "Job Title". Sort them by the "first_name" field in ascending
+ alphabetical order. Finally, LIMIT the results to 50
+ */
 SELECT
     id AS ID,
     CONCAT(first_name, ' ', last_name) AS "Full Name",
