ada746889c194348b9b03e6462cb2dab6fc2820b
Ilian Kostov <63207846+IlianDev@users.noreply.github.com>
2023-09-30 15:16:10 +0300

    Update 2_Concatenate.sql

--- Python DB - September 2023/2_Basic_CRUD/Exercise/2_Concatenate.sql
+++ Python DB - September 2023/2_Basic_CRUD/Exercise/2_Concatenate.sql
@@ -1,3 +1,10 @@
+/*
+ From the selected already data combine the "name" and "state", fields into a single field called "Cities Information".
+ Rename the "area" column as "Area (km2)".
+
+Submit your query for this task in the Judge system.
+ */
+
 SELECT
 	CONCAT(
 		name,
